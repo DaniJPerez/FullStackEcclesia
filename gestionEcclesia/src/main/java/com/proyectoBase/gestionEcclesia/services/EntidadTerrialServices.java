4e0b6d4014e4c52f0413544d32706fe69266499d
DaniJPerez <daniantimateria@gmail.com>
2026-01-19 22:44:22 -0500

    commit trabajo 19/01/2026 #2

--- gestionEcclesia/src/main/java/com/proyectoBase/gestionEcclesia/services/EntidadTerrialServices.java
+++ gestionEcclesia/src/main/java/com/proyectoBase/gestionEcclesia/services/EntidadTerrialServices.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class EntidadTerrialServices {
     private final EntidadTerritorialRepository entidadTerritorialRepository;
-
+    private final PaisServices paisServices;
 }
